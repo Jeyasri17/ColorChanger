fa5fe8520cd765bb6372dc7d1a936819af0ed534
Jeyasri17 <jeyasrivenkatarengan@gmail.com>
2022-12-20 10:58:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react";
+import ColorChanger from './Components/ColorChanger';
+function App(){
+  return(
+    <div>
+      <ColorChanger/>
+    </div>
+  )
+}
+export default App;
